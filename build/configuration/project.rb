7306eba5b04024d9c8c2ed7c030bfc96d75349fa
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-08-07 10:06:33 -0600

    Initial commit for the store project

--- build/configuration/project.rb
+++ build/configuration/project.rb
@@ -4,11 +4,10 @@ config =
   :project => "nothinbutdotnetstore",
   :target => "Debug",
   :source_dir => "source",
-  :app_dir => "source/nothinbutdotnetstore.web.ui",
+  :app_dir => delayed{File.join(configatron.source_dir,"nothinbutdotnetstore.web.ui")},
   :all_references => UniqueFiles.new(Dir.glob("packages/**/*.{dll,exe}")).all_files,
   :artifacts_dir => "artifacts",
   :config_dir => "source/config",
-  :app_dir => delayed{"source/#{configatron.project}"},
   :log_file_name => delayed{"#{configatron.project}_log.txt"},
   :log_level => "DEBUG"
 }

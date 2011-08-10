b34262b7e5de26c9809f08796c6fed89387b25ea
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-08-10 16:51:28 -0600

    Updated

--- source/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
+++ source/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
@@ -42,6 +42,27 @@ namespace nothinbutdotnetstore.specs
                 static IList<IProcessOneSpecificRequest> all_commands;
             }
 
+            public class and_it_does_not_have_the_command : when_finding_a_command_that_can_process_a_request
+            {
+                Establish c = () =>
+                {
+                    all_commands = Enumerable.Range(1, 100).Select(x => fake.an<IProcessOneSpecificRequest>()).ToList();
+                    request = fake.an<IContainRequestInformation>();
+                    the_null_command = fake.an<IProcessOneSpecificRequest>();
+                    depends.on<IEnumerable<IProcessOneSpecificRequest>>(all_commands);
+                    depends.on<MissingCommandFactory>(() => the_null_command);
+                };
+
+                Because b = () =>
+                    result = sut.get_command_for(request);
+
+                It should_return_the_special_case = () =>
+                    result.ShouldEqual(the_null_command);
+
+                static IList<IProcessOneSpecificRequest> all_commands;
+                static IProcessOneSpecificRequest the_null_command;
+            }
+
             static IProcessOneSpecificRequest result;
             static IContainRequestInformation request;
         }

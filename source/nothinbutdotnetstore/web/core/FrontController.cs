02d8a1b2f949a60f3cc696353ea4b9b42ea9cb40
arisj <aris.jardinero@sjrb.ca>
2011-08-10 16:25:47 -0600

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -1,7 +1,20 @@
-﻿namespace nothinbutdotnetstore.web.core
+﻿using System;
+
+namespace nothinbutdotnetstore.web.core
 {
     public class FrontController : IProcessWebRequests
     {
-         
+        readonly IFindCommandsThatCanProcessRequests command_registry;
+
+        public FrontController(IFindCommandsThatCanProcessRequests commandRegistry)
+        {
+            command_registry = commandRegistry;
+        }
+
+        public void process(IContainRequestInformation the_request)
+        {
+            IProcessOneSpecificRequest command = command_registry.get_command_for(the_request);
+            command.process(the_request);
+        }
     }
 }

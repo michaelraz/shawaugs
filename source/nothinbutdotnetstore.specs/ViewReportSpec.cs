7f374d083beb4727b083763e95f390c9ef680068
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-08-11 17:15:28 -0600

    Updated

--- source/nothinbutdotnetstore.specs/ViewReportSpec.cs
+++ source/nothinbutdotnetstore.specs/ViewReportSpec.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Machine.Specifications;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
@@ -8,10 +9,10 @@ using nothinbutdotnetstore.web.core;
 namespace nothinbutdotnetstore.specs
 {
     [Subject(typeof(ViewReport<>))]
-    public class ViewReportSpec<InputModel, ReportModel>
+    public class ViewReportSpec
     {
         public abstract class concern : Observes<IProcessAnApplicationBehaviour,
-                                            ViewReport<ReportModel>>
+                                            ViewReport<OurModel>>
         {
         }
 
@@ -20,24 +21,27 @@ namespace nothinbutdotnetstore.specs
             Establish c = () =>
             {
                 view_engine = depends.on<IRenderReports>();
+                the_query = depends.on<IQuery<OurModel>>();
                 request = fake.an<IContainRequestInformation>();
+                the_model = new OurModel();
 
-                the_query = fake.an<IQuery<ReportModel>>();
-                var repository = default(ReportModel);
-
+                the_query.setup(x => x.run( request)).Return(the_model);
             };
 
             Because b = () =>
                 sut.process(request);
 
             It should_pass_the_response_from_the_repository_to_the_view_engine_to_be_rendered = () =>
-                view_engine.received(x => x.render(repository));
+                view_engine.received(x => x.render(the_model));
 
             static IContainRequestInformation request;
             static IRenderReports view_engine;
-            static IQuery<ReportModel> the_query;
-            static InputModel model_type;
-            static ReportModel repository;
+            static IQuery<OurModel> the_query;
+            static OurModel the_model;
         }
+    }
+
+    public class OurModel
+    {
     }
 }
